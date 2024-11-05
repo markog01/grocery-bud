872d37b441df5dca92c81efbcc2027a87e915ec9
markog01 <markogligorevic01@gmail.com>
2024-11-05 19:11:34 +0100

    clear items done

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ let editID = "";
 // ****** EVENT LISTENERS **********
 // submit form
 form.addEventListener('submit', addItem);
+// clear items
+clearBtn.addEventListener('click', clearItems);
+
+
+
 // ****** FUNCTIONS **********
 function addItem(e) {
   e.preventDefault();
@@ -42,12 +47,21 @@ function addItem(e) {
               </button>
               
               </div> `;
+    const deleteBtn = element.querySelector('.delete-btn');
+    const editBtn = element.querySelector('.edit-btn');
+    deleteBtn.addEventListener('click', deleteItem);
+    editBtn.addEventListener('click', editItem);
+
    // append child
   list.appendChild(element);
     // display alert
   displayAlert('item added to the list', 'success');
   // show container
   container.classList.add('show-container');
+  //add to local storage
+  addToLocalStorage(id, value);
+  // set back to default
+  setBackToDefault(); 
   }
 
   else if (value && editFlag) {
@@ -71,6 +85,64 @@ function displayAlert(text, action) {
     alert.classList.remove(`alert-${action}`);
   }, 1000);
 }
+
+// delete function
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if (list.children.length === 0) {
+      container.classList.remove('show-container');
+  }
+  displayAlert('item removed','danger');
+  setBackToDefault();
+  // remove from local storage
+    // removeFromLocalStorage(id);
+}
+// edit function
+function editItem(e) {
+const element = e.currentTarget.parentElement.parentElement;
+// set edit item
+editElement = e.currentTarget.parentElement.previousElementSibling;
+// set form value
+grocery.value = editElement.innerHTML;
+editFlag = true;
+editID = element.dataset.id;
+submitBtn.textContent = 'edit';
+}
+
+// set back to default
+function setBackToDefault() {
+  grocery.value = '';
+  editFlag = false;
+  editID = '';
+  submitBtn.textContent = 'submit';
+}
+
+// clear items
+function clearItems() {
+    const items = document.querySelectorAll('.grocery-item');
+    
+    if (items.length > 0) {
+      items.forEach(function(item) {
+        list.removeChild(item);
+      });
+    }
+
+    container.classList.remove('show-container');
+    displayAlert('empty list', 'danger');
+    setBackToDefault();
+    // localStorage.removeItem('list');
+  }
+
+
 // ****** LOCAL STORAGE **********
+function addToLocalStorage(id,value) {
+  console.log('added to local storage');
+}
+
+function removeFromLocalStorage(id) {
+  
+}
 
 // ****** SETUP ITEMS **********
